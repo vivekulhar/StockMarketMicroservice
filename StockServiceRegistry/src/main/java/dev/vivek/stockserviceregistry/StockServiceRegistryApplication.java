53eb2631538340b19ae6220d963bd55414a35336
Vivek Kulhar <vivek.kulhar23@gmail.com>
2023-11-24 23:20:09 +0530

    added some comments

--- StockServiceRegistry/src/main/java/dev/vivek/stockserviceregistry/StockServiceRegistryApplication.java
+++ StockServiceRegistry/src/main/java/dev/vivek/stockserviceregistry/StockServiceRegistryApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class StockServiceRegistryApplication {
-
+    // good
     public static void main(String[] args) {
         SpringApplication.run(StockServiceRegistryApplication.class, args);
     }
